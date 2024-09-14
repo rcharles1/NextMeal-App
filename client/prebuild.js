e01bc41de6d85af2167301487a05daabf1b01339
Robin Charles <robincharles940@gmail.com>
2024-09-14 18:30:16 +0300

    tweaks

--- client/prebuild.js
+++ client/prebuild.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
 
-const filePath = path.join(__dirname, 'client', 'node_modules', 'mongodb', 'lib', 'index.js');
+const filePath = path.join(process.cwd(), 'client', 'node_modules', 'mongodb', 'lib', 'index.js');
 const fileContent = fs.readFileSync(filePath, 'utf8');
 
 const updatedContent = fileContent.replace(/require\('fs\/promises'\)/g, 'null');
