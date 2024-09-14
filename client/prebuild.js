adf5a4330b9356c02413c6ab3a34181d695b2cc2
Robin Charles <robincharles940@gmail.com>
2024-09-14 21:32:02 +0300

    tweaks v2

--- client/prebuild.js
+++ client/prebuild.js
@@ -1,10 +1,13 @@
 import fs from 'fs';
 import path from 'path';
 
-const filePath = path.join(process.cwd(), 'client', 'node_modules', 'mongodb', 'lib', 'index.js');
-const fileContent = fs.readFileSync(filePath, 'utf8');
+const filePath = path.resolve('client', 'node_modules', 'mongodb', 'lib', 'index.js');
 
-const updatedContent = fileContent.replace(/require\('fs\/promises'\)/g, 'null');
-
-fs.writeFileSync(filePath, updatedContent, 'utf8');
-console.log('Removed problematic imports from index.js');
+if (fs.existsSync(filePath)) {
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    const updatedContent = fileContent.replace(/require\('fs\/promises'\)/g, 'null');
+    fs.writeFileSync(filePath, updatedContent, 'utf8');
+    console.log('Removed problematic imports from index.js');
+} else {
+    console.error('File does not exist:', filePath);
+}
