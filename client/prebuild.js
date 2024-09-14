e1d1a1bd3664685c87cf32b11c53a8621fef633f
Robin Charles <robincharles940@gmail.com>
2024-09-14 21:51:34 +0300

    tweaks v4

--- client/prebuild.js
+++ client/prebuild.js
@@ -2,15 +2,34 @@ import fs from 'fs';
 import path from 'path';
 
 const isVercel = process.env.VERCEL === '1';
-const filePath = isVercel 
+
+// Paths for the problematic files
+const mongodbFilePath = isVercel 
     ? path.resolve('/vercel/path0/client/node_modules/mongodb/lib/index.js')
     : path.resolve('client', 'node_modules', 'mongodb', 'lib', 'index.js');
 
-if (fs.existsSync(filePath)) {
-    const fileContent = fs.readFileSync(filePath, 'utf8');
-    const updatedContent = fileContent.replace(/require\('fs\/promises'\)/g, 'null');
-    fs.writeFileSync(filePath, updatedContent, 'utf8');
-    console.log('Removed problematic imports from index.js');
+const emptyJsFilePath = isVercel 
+    ? path.resolve('/vercel/path0/client/node_modules/node-stdlib-browser/esm/mock/empty.js')
+    : path.resolve('client', 'node_modules', 'node-stdlib-browser', 'esm', 'mock', 'empty.js');
+
+// Function to update file content
+const updateFileContent = (filePath, searchValue, replaceValue) => {
+    if (fs.existsSync(filePath)) {
+        const fileContent = fs.readFileSync(filePath, 'utf8');
+        const updatedContent = fileContent.replace(searchValue, replaceValue);
+        fs.writeFileSync(filePath, updatedContent, 'utf8');
+        console.log(`Updated content in ${filePath}`);
+    } else {
+        console.error('File does not exist:', filePath);
+    }
+};
+
+// Update mongodb index.js file
+updateFileContent(mongodbFilePath, /require\('fs\/promises'\)/g, 'null');
+
+// Handle the ENOTDIR error by ensuring the path is correct
+if (fs.existsSync(emptyJsFilePath) && fs.lstatSync(emptyJsFilePath).isFile()) {
+    console.log(`File exists and is a file: ${emptyJsFilePath}`);
 } else {
-    console.error('File does not exist:', filePath);
+    console.error('File does not exist or is not a file:', emptyJsFilePath);
 }
