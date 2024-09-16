ac0188c6aa28b16f083259304e4ca193dffaafb7
Robin Charles <robincharles940@gmail.com>
2024-09-16 19:13:30 +0300

    introduced a process webpack plugin

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import NodePolyfillPlugin from 'node-polyfill-webpack-plugin';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    NodePolyfillPlugin(),
+  ],
   resolve: {
     alias: {
       crypto: 'crypto-browserify',
