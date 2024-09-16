5a002f4fb531af37dff0a68b5b7003bc3304f20b
Robin Charles <robincharles940@gmail.com>
2024-09-16 19:25:06 +0300

    tweaks 2 v5

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { NodePolyfillPlugin }from 'node-polyfill-webpack-plugin';
+import NodePolyfillPlugin from 'node-polyfill-webpack-plugin';
 
 export default defineConfig({
   plugins: [
     react(),
-    NodePolyfillPlugin(),
+    new NodePolyfillPlugin(),
   ],
   resolve: {
     alias: {
