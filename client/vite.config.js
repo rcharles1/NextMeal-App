e4a192b3b03a1ef165c78d81af36549c77e62a13
Robin Charles <robincharles940@gmail.com>
2024-09-16 15:35:59 +0300

    Introduced process polyfill

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      crypto: 'crypto-browserify'
+      crypto: 'crypto-browserify',
+      process: 'process/browser',
     }
   }
 });
