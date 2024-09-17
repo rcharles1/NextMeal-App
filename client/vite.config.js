0a15b9b56e91b54ef39e16093bd4a7469a8e6b78
Robin Charles <robincharles940@gmail.com>
2024-09-17 12:41:41 +0300

    tweaks 2 v8

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 
 export default defineConfig({
   plugins: [
@@ -9,18 +10,21 @@ export default defineConfig({
   resolve: {
     alias: {
       crypto: 'crypto-browserify',
+      stream: 'stream-browserify',
     }
   },
   optimizeDeps: {
     esbuildOptions: {
       define: {
         global: 'globalThis',
+        process: 'process',
       },
       plugins: [
         NodeGlobalsPolyfillPlugin({
           process: true,
           buffer: true,
         }),
+        NodeModulesPolyfillPlugin(),
       ],
     },
   },
